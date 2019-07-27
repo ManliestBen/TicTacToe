5f3e4a3236c219b84d355e93d0a45ad5cbf39940
ManliestBen <benjamintmanley@gmail.com>
2019-07-27 14:50:12 -0500

    Fix board array, rename idx variable

--- js/main.js
+++ js/main.js
@@ -36,9 +36,7 @@ document.querySelector('section.board').addEventListener('click', onClick);
 init();
 
 function init() {
-    board = [
-        ['null', 'null', 'null', 'null', '1', 'null', 'null', 'null', 'null']
-    ];
+    board = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
     turn = 1;
     winner = null;
     
@@ -47,9 +45,9 @@ function init() {
 
 
 function onClick(evt){
-    let squareNum = parseInt(evt.target.id.replace('sq',''));
-    console.log(squareNum);
-    render(squareNum);
+    let squareIdx = parseInt(evt.target.id.replace('sq',''));
+    console.log(squareIdx);
+    render(squareIdx);
 
 }
 
@@ -58,17 +56,12 @@ function getWinner(){
     //run three functions (chkRow, chkCol, chkDiag) to determine if there is a winner
     //(winner would be assigned if absolute value of row/col/diag total is 3)
 }
-function render(squareNum){
-    console.log(squareNum);    
-    // console.log(`sq${squareNum}`);
-    // let div = "sq2"
+function render(squareIdx){
     
-    document.getElementById(`sq${squareNum}`).style.backgroundColor = SQUARE_STATE[turn];
-    // let div = document.getElementById(`sq${squareNum}`);
-    // div.style.backgroundColor = 'white';
-    // div.style.backgroundColor = SQUARE_STATE[turn];
-    // console.log(div);
+    console.log(squareIdx);    
+    document.getElementById(`sq${squareIdx}`).style.backgroundColor = SQUARE_STATE[turn];
     console.log('turn' + turn);
     console.log(SQUARE_STATE[turn]);
+    board[squareIdx] = turn;
     turn *= -1;
 }
