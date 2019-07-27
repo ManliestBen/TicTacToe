90819f4dcd2e8d4b30368200c2d9fdd548c9c469
ManliestBen <benjamintmanley@gmail.com>
2019-07-27 12:13:32 -0500

    Start pseudocode, initialize variables

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let test = "Test";
+console.log(test);
+
+/*------Constants------*/
+
+const SQUARE_STATE = {
+    'null' = 'white',
+    '1' = 'green',
+    '-1' = 'yellow'
+};
+
+
+
+/*------Variables (state)------*/
+
+let board, turn, winner;
+
+
+
+
+/*------Cached Element References------*/
+
+
+
+
+
+
+/*------Event Listeners------*/
+
+
+
+
+
+/*------Functions------*/
