db75a3dc7ea77451bb280683a6e4e63f0dffc4f1
ManliestBen <benjamintmanley@gmail.com>
2019-07-27 18:06:31 -0500

    Add turn info display

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@
 
 const SQUARE_STATE = {
     'null': 'white',
-    '1': 'green',
-    '-1': 'yellow'
+    '1': 'green',       // 1 is X
+    '-1': 'yellow'      // -1 is O
 };
 
 
@@ -19,7 +19,7 @@ let board, turn, winner;
 
 /*------Cached Element References------*/
 
-
+let gameStatus = document.getElementById('message');
 
 
 
@@ -38,6 +38,7 @@ function init() {
     board = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
     turn = 1;
     winner = null;
+    gameStatus.textContent = "It is X's turn!"
     
     // render();
 }
@@ -56,6 +57,9 @@ function getWinner(){
     //run three functions (chkRow, chkCol, chkDiag) to determine if there is a winner
     //(winner would be assigned if absolute value of row/col/diag total is 3)
 }
+
+
+
 function render(squareIdx){
     console.log(squareIdx);    
     setLetter = document.getElementById(`sq${squareIdx}`);
@@ -65,8 +69,10 @@ function render(squareIdx){
     board[squareIdx] = turn;
     if (turn === 1) {
         setLetter.textContent = "X";
+        gameStatus.textContent = "It is O's turn!"
     }   else {
         setLetter.textContent = "O";
+        gameStatus.textContent = "It is X's turn!"
 }
     turn *= -1;
 }
