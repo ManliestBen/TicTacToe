5aeb064f49d45dbbe8bbf82eb2f5de6fb44aacd3
ManliestBen <benjamintmanley@gmail.com>
2019-07-28 12:51:41 -0500

    Add confetti on win

--- js/main.js
+++ js/main.js
@@ -68,12 +68,14 @@ function getWinner(){
     if (board[0]+board[1]+board[2] === 3 ||  board[3]+board[4]+board[5] === 3 ||  board[6]+board[7]+board[8] === 3 ||  board[0]+board[3]+board[6] === 3 ||  board[1]+board[4]+board[7] === 3 ||  board[2]+board[5]+board[8] === 3 ||  board[0]+board[4]+board[8] === 3 ||  board[2]+board[4]+board[6] === 3){
         gameStatus.textContent = "X wins the game!";
         document.getElementById("board").className += " hvr-buzz";
+        confetti.start(1500);
         isWinner = true;
     } 
     
     if (board[0]+board[1]+board[2] === -3 ||  board[3]+board[4]+board[5] === -3 ||  board[6]+board[7]+board[8] === -3 ||  board[0]+board[3]+board[6] === -3 ||  board[1]+board[4]+board[7] === -3 ||  board[2]+board[5]+board[8] === -3 ||  board[0]+board[4]+board[8] === -3 ||  board[2]+board[4]+board[6] === -3){
         gameStatus.textContent = "O wins the game!";
         document.getElementById("board").className += " hvr-buzz";
+        confetti.start(1500);
         isWinner = true;
     }
     
